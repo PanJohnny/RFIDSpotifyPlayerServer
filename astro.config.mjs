4533dbe0729e2561f9eb67db4aeeb6b8bd5ab928
PanJohnny <devstefancz@gmail.com>
2022-12-21 21:51:00 +0100

    made it work

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,20 @@
 import { defineConfig } from 'astro/config';
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url);
+
+// https://astro.build/config
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-    output: "server"
+  output: "server",
+  adapter: node({ mode: "standalone" }),
+  vite: {
+    resolve: {
+      alias: {
+        fs: require.resolve('rollup-plugin-node-builtins'),
+      }
+    }
+  }
 });
--- src/util.ts
+++ src/util.ts
@@ -1,7 +1,45 @@
 import SpotifyWebApi from "spotify-web-api-node";
+import * as fs from 'fs';
 
-export const spotifyApi:SpotifyWebApi = new SpotifyWebApi({
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const path = __dirname + "\\..\\..\\temp_data.json";
+
+export const spotifyApi: SpotifyWebApi = new SpotifyWebApi({
 	clientId: "b68e2f3f6ea1445585f99b6a41be6db2",
 	clientSecret: "12e2f1fb2edd44f28600513e315a75c6",
-	redirectUri: "http://localhost:3000/callback"
+	redirectUri: "http://10.0.0.17:3000/callback"
 })
+
+export interface TempData {
+	writing?: boolean;
+	data?: string;
+	card?: string;
+}
+export function writeData(data: TempData) {
+	const begin = JSON.parse(fs.readFileSync(path).toString("utf-8"))
+
+	if (data.writing !== undefined) {
+		begin.writing = data.writing;
+	}
+
+
+	if (data.data) {
+		begin.data = data.data;
+
+	}
+
+	if (data.card) {
+		begin.card = data.card;
+	}
+
+	fs.writeFileSync(path, JSON.stringify(begin))
+}
+
+export function readData(): TempData {
+	return JSON.parse(fs.readFileSync(path).toString("utf-8"))
+}
